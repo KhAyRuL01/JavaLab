156c2e548bb28914f06510d129927d1847c96609
KhAyRuL01 <khairulf16b@gmail.com>
2017-10-16 00:14:38 +0600

    this is incomplete

---
+++ LamdaExpression/src/Lamda/LamdaExp.java
@@ -0,0 +1,15 @@
+package Lamda;
+
+interface lamda
+{
+	void show();
+}
+
+public class LamdaExp {
+	public static void main(String[] args)
+	{
+		lamda obj = () ->  System.out.print("hello");
+		landa.show();
+		System.out.print("hello");
+	}
+}
